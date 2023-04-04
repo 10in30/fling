0b9d02bcfea61070e7cf60dff95fc1af0152ef47
Anouk Ruhaak <anoukruhaak@Anouks-MacBook-Pro.local>
2023-04-04 16:59:19 -0700

    retrieve all domains for for a phrase

--- namefinder.py
+++ namefinder.py
@@ -13,40 +13,54 @@ openai.api_key = environ["OPENAI_API_KEY"]
 name_token_dev = environ["NAME_TOKEN_DEV"]
 name_user = environ["NAME_USER"]
 
-def models():
-    headers = {'Authorization': f'Bearer {openai.api_key}'}
-    r = httpx.get("https://api.openai.com/v1/models", headers=headers )
-    pprint(r.url)
-    return json.loads(r.content)
 
 def get_company_name_for(description):
     response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(description),
-            temperature=0.6,
-        )
-    return response["choices"][0].get("text")
-
+        model="text-davinci-003",
+        prompt=generate_prompt(description),
+        temperature=0,
+        n=1
+    )
+    return response["choices"][0].get("text").split(",")
 
 
 def generate_prompt(description):
     return """Suggest three names for a company based on these descriptors. Name should be one word long.
 Description: {}
-Names:""".format(
+Only respond with the three names, with a comma and space between each. For example:
+Name 1, Name 2, Name 3
+
+""".format(
         description.capitalize()
     )
 
-def find_domain(phrase):
+
+def query_name_for_domains(phrase):
+    print(phrase)
     params = f'{{"keyword":"{phrase}"}}'
     headers = {'Content-Type': 'application/json'}
 
-    r = httpx.post("https://api.dev.name.com/v4/domains:search", data=params
-                   , auth=(name_user, name_token_dev), headers=headers)
+    r = httpx.post("https://api.dev.name.com/v4/domains:search",
+                   data=params, auth=(name_user, name_token_dev), headers=headers)
 
-    return(json.loads(r.content))
+    return (json.loads(r.content))
+
 
 def is_domain_free(domains):
-    pprint(domains['results'])
-    free_domains = [(d.get('domainName'), d.get('purchasable'), d.get('purchasePrice'), d.get('tld')) for d in domains['results'] if d.get('purchasable') is True and d.get('tld') in ["com", "net", "io", "co", "ai", "info", "xyz", "org"]]
+    allowed_tlds = ["com", "net", "io", "co", "ai", "info", "xyz", "org"]
+    free_domains = [{"domain": d.get('domainName'), "available": d.get('purchasable'), "price": f"{d.get('purchasePrice')} USD"} 
+                    for d in domains['results'] if d.get(
+        'purchasable') is True and d.get('tld') in allowed_tlds]
+    if free_domains:
+        return free_domains
+    else:
+        return []
+
+
+def get_all_domains(phrase):
+    companies = get_company_name_for(phrase)
+    domains = [query_name_for_domains(c) for c in companies]
+    free_domains = [is_domain_free(d) for d in domains]
     return free_domains
+
 
