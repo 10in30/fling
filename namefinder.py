622de39a87c65f9bbaa71a91743e7670af9df449
Anouk Ruhaak <anoukruhaak@Anouks-MacBook-Pro.local>
2023-04-04 15:11:40 -0700

    find free domains

--- namefinder.py
+++ namefinder.py
@@ -5,10 +5,13 @@ from os import environ
 from pprint import pprint
 import requests
 import httpx
+import base64
 
 load_dotenv()
 
 openai.api_key = environ["OPENAI_API_KEY"]
+name_token_dev = environ["NAME_TOKEN_DEV"]
+name_user = environ["NAME_USER"]
 
 def models():
     headers = {'Authorization': f'Bearer {openai.api_key}'}
@@ -32,4 +35,18 @@ Description: {}
 Names:""".format(
         description.capitalize()
     )
+
+def find_domain(phrase):
+    params = f'{{"keyword":"{phrase}"}}'
+    headers = {'Content-Type': 'application/json'}
+
+    r = httpx.post("https://api.dev.name.com/v4/domains:search", data=params
+                   , auth=(name_user, name_token_dev), headers=headers)
+
+    return(json.loads(r.content))
+
+def is_domain_free(domains):
+    pprint(domains['results'])
+    free_domains = [(d.get('domainName'), d.get('purchasable'), d.get('purchasePrice'), d.get('tld')) for d in domains['results'] if d.get('purchasable') is True and d.get('tld') in ["com", "net", "io", "co", "ai", "info", "xyz", "org"]]
+    return free_domains
 
