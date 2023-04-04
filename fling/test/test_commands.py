da131f6c9cfb29fcc2e5ce1af6d22153839f72a2
Joshua McKenty <jmckenty@gmail.com>
2023-04-04 12:27:39 -0700

    Rename test to the right thing.

--- fling/test/test_commands.py
+++ fling/test/test_commands.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from fling.bin.fling import fling
 
 
-def test_greet_cli():
+def test_fling_search_cli():
     runner = CliRunner()
     result = runner.invoke(fling, ["search", "foo"])
     assert result.exit_code == 0
