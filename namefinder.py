f96909577d875df6fbeeb6aa179024c533fb3ed5
Anouk Ruhaak <anoukruhaak@Anouks-MacBook-Pro.local>
2023-04-04 13:27:10 -0700

    clean up cpde

--- namefinder.py
+++ namefinder.py
@@ -10,35 +10,26 @@ load_dotenv()
 
 openai.api_key = environ["OPENAI_API_KEY"]
 
-def get():
+def models():
     headers = {'Authorization': f'Bearer {openai.api_key}'}
     r = httpx.get("https://api.openai.com/v1/models", headers=headers )
     pprint(r.url)
     return json.loads(r.content)
 
-def post():
-    headers = {'Authorization': f'Bearer {openai.api_key}'}
-    params = {"model": "text-davinci-003",
-    "prompt": "Say this is a test",
-    "max_tokens": 7,
-    "temperature": 0}
-    r = httpx.post("https://api.openai.com/v1/completions", headers=headers, params=params)
-
-def completion_post(word):
+def get_company_name_for(description):
     response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(word),
+            prompt=generate_prompt(description),
             temperature=0.6,
         )
     return response["choices"][0].get("text")
 
 
 
-def generate_prompt(word):
+def generate_prompt(description):
     return """Suggest three names for a company based on these descriptors. Name should be one word long.
 Description: {}
 Names:""".format(
-        word.capitalize()
+        description.capitalize()
     )
-
 
