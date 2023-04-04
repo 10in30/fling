30d2579205a733791492cf51c200c0a76e7e52dc
Joshua McKenty <jmckenty@gmail.com>
2023-04-04 13:28:26 -0700

    Moved boilerplate.

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello, flinger!'.encode('utf-8'))
+        self.wfile.write(json.dumps(
+            {'hello': 'Hello, flinger!'}).encode(encoding='utf_8'))
         return
---
+++ fling/lib.py
@@ -0,0 +1,27 @@
+"""Main libraries for side project management"""
+
+import os
+import pyyaml
+from dotenv import loadenv
+
+loadenv()
+
+BOILERPLATEDIR = os.environ.get("BOILERPLATEDIR", "boilerplate")  # JMC: TODO: map to installed directory or relative to this file
+
+class FlingConfig:
+    config: object = {}
+    config_path: str = None
+
+    def __init__(self, config_path='flask.yaml'):
+        self.config_path = config_path
+        self.load()
+
+    def load(self):
+        if not os.path.exists(self.config_path):
+
+        with open(self.config_path, "r+") as config_file:
+            self.config = pyyaml.loads(config_file.read())
+
+    def write(self):
+        with open(self.config_path, "w+") as config_file:
+            config_file.write(pyyaml.dumps(self.config))
