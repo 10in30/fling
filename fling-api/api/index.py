649414a725572c029ee8fb5c760abc2056915085
Joshua McKenty <jmckenty@gmail.com>
2023-04-05 20:21:58 -0700

    Small fix for github logins.

--- fling-api/api/index.py
+++ fling-api/api/index.py
@@ -38,7 +38,7 @@ async def github_login():
 
 
 @app.get('/github-code')
-async def github_code(code:str):
+async def github_code(code: str):
     params = {
         'client_id': github_dev_client_id,
         'client_secret': github_dev_client_secret,
@@ -47,14 +47,22 @@ async def github_code(code:str):
 
     headers = {'Accept': 'application/json'}
     async with httpx.AsyncClient() as client:
-        response = await client.post(url="https://github.com/login/oauth/access_token", params=params, headers=headers)
+        response = await client.post(
+            url="https://github.com/login/oauth/access_token",
+            params=params,
+            headers=headers)
     response_json = response.json()
     access_token = response_json['access_token']
 
     async with httpx.AsyncClient() as client:
         headers.update({'Authorization': f'Bearer {access_token}'})
-        response = await client.get(url="https:api.github.com/user", headers=headers)
-    return response.json()
+        response = await client.get(url="https://api.github.com/user", headers=headers)
+        username = response.json()['login']
+        repo_list = await client.get(
+            url=f"https://api.github.com/search/repositories?q=user:{username}",
+            headers=headers)
+            # %20is:private
+        return repo_list.json()
 
 
 @app.post("/{fling_id}/add", tags=["data"])
