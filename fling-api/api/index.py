50b79b036a4f0bd10dca6c80e5bac929709130df
Joshua McKenty <jmckenty@gmail.com>
2023-05-14 12:41:16 -0700

    Support multiple domains at once.

--- fling-api/api/index.py
+++ fling-api/api/index.py
@@ -87,20 +87,27 @@ def change_txt_record(
     action: str, validation_domain_name: str, validation: str, ttl: str
 ) -> str:
     zone_id = _find_zone_id_for_domain(validation_domain_name)
-    # rrecords = [r for r in r53.get_all_rrsets(zone_id)]
+    paginator = r53.get_paginator('list_resource_record_sets')
+    rrecords = []
+    for page in paginator.paginate(HostedZoneId=zone_id):
+        for rrset in page['ResourceRecordSets']:
+            if rrset['Name'].split('.')[0] == '_acme-challenge':
+                rrecords = rrset['ResourceRecords']
+                break
+        if rrecords:
+            break
     challenge = {"Value": '"{0}"'.format(validation)}
-    rrecords = [challenge]
-    # if action == "DELETE":
-    #     # Remove the record being deleted from the list of tracked records
-    #     rrecords.remove(challenge)
-    #     if rrecords:
-    #         # Need to update instead, as we're not deleting the rrset
-    #         action = "UPSERT"
-    #     else:
-    #         # Create a new list containing the record to use with DELETE
-    #         rrecords = [challenge]
-    # else:
-    #     rrecords.append(challenge)
+    if action == "DELETE":
+        # Remove the record being deleted from the list of tracked records
+        rrecords.remove(challenge)
+        if rrecords:
+            # Need to update instead, as we're not deleting the rrset
+            action = "UPSERT"
+        else:
+            # Create a new list containing the record to use with DELETE
+            rrecords = [challenge]
+    else:
+        rrecords.append(challenge)
 
     response = r53.change_resource_record_sets(
         HostedZoneId=zone_id,
